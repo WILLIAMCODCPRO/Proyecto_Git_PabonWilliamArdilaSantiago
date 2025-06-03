b98476971c2eee8dc91210c2b7a1bf6fdcce480d
William Pabon <williamcodcpro@gmail.com>
2025-06-03 12:42:33 -0500

    chore: :sparkles: added main.py whit initial menu structuture

---
+++ main.py
@@ -0,0 +1,35 @@
+boolianito = True
+
+while(boolianito):
+
+ print("=============================================")
+ print("         Gestion de Empanadas                ")
+ print("=============================================")
+ print("1. Registar un nuevo tipo de empanada")
+ print("2. Modificar un tipo de empanada")
+ print("3. Eliminar un tipo de empanada")
+ print("=============================================")
+ 
+ opcion = int(input("Que accion deseas realizar \n"))
+
+ print("=============================================")
+ print("         Registro de Empanadas               ")
+ print("=============================================")
+ 
+ print("=============================================")
+
+ print("=============================================")
+ print("         Modificacion de Empanadas           ")
+ print("=============================================")
+
+ print("=============================================")
+
+ print("=============================================")
+ print("         Eliminar Empanadas                  ")
+ print("=============================================")
+
+ print("=============================================")
+
+
+
+ 
